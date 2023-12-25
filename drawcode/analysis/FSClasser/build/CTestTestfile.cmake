0456f7b7c8219491ee72f555dc58a630e7d964ce
oiunun <a1261926744@163.com>
2023-12-25 16:46:20 +0800

    clean

--- drawcode/analysis/FSClasser/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workfs2/bes/lig/higgs/FSClasser
-# Build directory: /workfs2/bes/lig/higgs/FSClasser/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
